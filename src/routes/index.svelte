d09da69b36d42f45033b7421fefa55b561f2f4f4
abel-HubArch <eabel4129@gmail.com>
2021-06-12 02:28:37 -0400

    Anadi la actualizacion2

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,5 @@
-<h1>Hola</h1>
+<script>
+	export let segment;
+</script>
+<h1>El Oyente</h1>
+<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="noticias/cancilleria-de-peru-envia-nota-de-protesta-a-bolivia">about</a></li>
