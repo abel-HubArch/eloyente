54c97aa82cc3f319629b1410d7e03e52e6527f9e
abel-HubArch <eabel4129@gmail.com>
2021-06-15 22:06:53 -0400

    Subir estos cambios a aqui

---
+++ src/components/Redes-sociales.svelte
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+<style>
+
+</style>
+<div class="Redes_sociales">
+    <h2>Escribeme aca</h2>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,11 @@
 <script>
+	import RedesSociales from './../components/Redes-sociales.svelte';
 	export let segment;
 </script>
 <h1>El Oyente</h1>
+
+<h2>Redes sociales</h2>
+<RedesSociales></RedesSociales>
+
+
 <li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="noticias/cancilleria-de-peru-envia-nota-de-protesta-a-bolivia">about</a></li>
